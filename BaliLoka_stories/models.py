8d0d65ff00c89231abafd75f06e00bc457fc9f14
Adiieeee <adiansyah.nugraha@gmail.com>
2024-10-23 16:03:26 +0700

    last push sebelum masuk ke ui

--- BaliLoka_stories/models.py
+++ BaliLoka_stories/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class StoriesEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     image = models.ImageField()
     description = models.TextField(blank=True, null=True)
