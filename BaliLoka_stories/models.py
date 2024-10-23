949ccdea1011403a6ec109c7f07886297decfc08
Adiieeee <adiansyah.nugraha@gmail.com>
2024-10-23 15:13:22 +0700

    coba

--- BaliLoka_stories/models.py
+++ BaliLoka_stories/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Stories(models.Model):
+class StoriesEntry(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
     image = models.ImageField()
-    
+    description = models.TextField(blank=True, null=True)
+
     def __str__(self):
         return self.name
