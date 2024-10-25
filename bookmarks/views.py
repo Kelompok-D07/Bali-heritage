17531e5d90af48ef714b2116c88dbfdd40976113
z41d123@gmail.com <z41d123@gmail.com>
2024-10-25 17:19:31 +0700

    TESTING PUSH

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from bookmarks.models import Bookmark
+from Homepage.models import Category
 from django.http import HttpResponse
 from django.core import serializers
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
-# @login_required()
+@login_required(login_url='/')
 def show_bookmarks(request):
-    # bookmarks = Bookmark.objects.filter(user=request.user)
-    context = {'bookmarks': "name"}
+    bookmarks = Bookmark.objects.filter(user=request.user)
+    categories = Category.objects.all()
+    context = {'bookmarks': bookmarks,
+               'categories': categories}
     return render(request, "bookmarks.html", context)
