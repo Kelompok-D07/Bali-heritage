693ae2a66b1faaf3a18f6a329bcb75b5792a82f2
Bastian2312 <bastian.adiputras@gmail.com>
2024-10-26 03:13:35 +0700

    Added: Restaurant html

--- Homepage/urls.py
+++ Homepage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from Homepage.views import show_main
+from Homepage.views import show_main, restaurant
 
 app_name = 'Homepage'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('restaurant/', restaurant, name='restaurant'),
 ]
