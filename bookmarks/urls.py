01c7c1884071c9208245c1dca6975746c13f8b86
z41d123@gmail.com <z41d123@gmail.com>
2024-10-25 23:09:40 +0700

    Add function delete Bookmarks

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from bookmarks.views import show_bookmarks
+from bookmarks.views import show_bookmarks,delete_bookmarks_item
 
 app_name = 'bookmarks'
 
 urlpatterns = [
     path('', show_bookmarks, name='show_bookmarks'),
+    path('delete-bookmarks-item/<int:item_id>/', delete_bookmarks_item, name='delete_bookmarks_item'),
 ]
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from bookmarks.models import Bookmark
 from Homepage.models import Category
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+
 
 @login_required(login_url='/')
 def show_bookmarks(request):
@@ -12,3 +14,21 @@ def show_bookmarks(request):
     context = {'bookmarks': bookmarks,
                'categories': categories}
     return render(request, "bookmarks.html", context)
+
+
+@csrf_exempt
+def delete_bookmarks_item(request, item_id):
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            try:
+                item = Bookmark.objects.get(pk=item_id, user=request.user)
+                item_name = item.product.name
+                item.delete()
+                return JsonResponse({'status': item_name + " removed from Bookmarks"})
+            except Bookmark.DoesNotExist:
+                return JsonResponse({'status': item_name + " not found in Bookmarks"}, status=404)
+        else:
+            return JsonResponse({'status': "Login to continue"}, status=403)
+    return JsonResponse({'status': "Invalid request method"}, status=400)
+
+
