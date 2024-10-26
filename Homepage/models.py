20a1346cd42db1c772dc252fafbd7b1c6fbe9cbd
Bastian2312 <bastian.adiputras@gmail.com>
2024-10-27 02:18:23 +0700

    outlined category feature

--- Homepage/models.py
+++ Homepage/models.py
@@ -4,6 +4,7 @@ import uuid
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category_images/')
 
     def __str__(self):
         return self.name
