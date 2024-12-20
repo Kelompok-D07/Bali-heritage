106342aef570b466c43dbdcb15c65633b715226c
Bastian2312 <bastian.adiputras@gmail.com>
2024-12-20 22:42:28 +0700

    fix: login issue

--- Homepage/urls.py
+++ Homepage/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Homepage.views import show_main, restaurant, filter_product
+from Homepage.views import show_main, restaurant, filter_product,create_product_flutter, get_categories, get_restaurants
 
 app_name = 'Homepage'
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('restaurant/', restaurant, name='restaurant'),
     path('filter-product/', filter_product, name='filter_products'),  # URL untuk filtering
+    path('create-product-flutter/', create_product_flutter, name='create_product_flutter'),
+    path('get-categories/', get_categories, name='get_categories'),
+    path('get-restaurants/', get_restaurants, name='get_restaurants'),
 ]
